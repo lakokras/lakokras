5c94b8725f4161bef0fa3a3d8055c6982c65fc9c
Arthur <chupryna.2004@mail.ru>
2023-12-02 19:04:40 +0300

    psql

--- src/main/java/com/example/lakokras/controller/UserController.java
+++ src/main/java/com/example/lakokras/controller/UserController.java
@@ -1,18 +1,37 @@
 package com.example.lakokras.controller;
 
 
+import com.example.lakokras.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import repository.UserRepo;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @PostMapping
+    public ResponseEntity registration(@RequestBody UserEntity user) {
+        try {
+            if (userRepo.findByUsername(user.getUsername()) != null) {
+                return ResponseEntity.badRequest().body("Пользователь с таким именем уже существует");
+            }
+            userRepo.save(user);
+            return ResponseEntity.ok("Пользователь успешно сохранен");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Произошла ошибка");
+        }
+
+    }
+
     @GetMapping
     public ResponseEntity getUsers() {
         try {
-            return ResponseEntity.ok("Сервер работает!");
+            return ResponseEntity.ok("Сервер работает");
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Произошла ошибка");
         }
--- src/main/java/com/example/lakokras/entity/UserEntity.java
+++ src/main/java/com/example/lakokras/entity/UserEntity.java
@@ -1,4 +1,46 @@
 package com.example.lakokras.entity;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.annotation.Id;
+
+@EntityScan
 public class UserEntity {
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    public UserEntity(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserEntity(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
